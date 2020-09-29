e9219de951cc0eac3fd8b050b5268e67dc52dd8a
Florian Schmidt <florian.schmidt@platis.de>
2020-09-29 09:41:44 +0200

    Use kotlin 1.4 and mathcat 1.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@
 
 plugins {
     //java
-    kotlin("multiplatform") version "1.3.70"
+    kotlin("multiplatform") version Config.Versions.kotlin
     id ("com.github.hierynomus.license") version "0.15.0"
     `maven-publish`
     maven
@@ -27,7 +27,7 @@ kotlin {
         dependencies {
             implementation(kotlin("stdlib-jdk8"))
             implementation(kotlin("reflect"))
-            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5")
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Config.Versions.coroutines}")
         }
     }
     // JVM-specific tests and their dependencies:
@@ -41,7 +41,7 @@ kotlin {
         dependencies {
             //implementation(kotlin("js"))
             implementation(kotlin("reflect"))
-            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.3.5")
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:${Config.Versions.coroutines}")
         }
         /* ... */
     }
@@ -52,7 +52,7 @@ kotlin {
             dependencies {
                 implementation(kotlin("stdlib-common"))
                 implementation(kotlin("reflect"))
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-common:1.3.5")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${Config.Versions.coroutines}")
             }
         }
         val commonTest by getting {
@@ -63,144 +63,3 @@ kotlin {
         }
     }
 }
-
-
-
-/*
-dependencies {
-    implementation( Config.Dependencies.kotlinStandardLibrary )
-    implementation( Config.Dependencies.coroutines )
-    implementation( kotlin("reflect") )
-
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.3.2")
-    testImplementation("junit", "junit", "4.12")
-}
-
-configure<JavaPluginConvention> {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-}
-*/
-/*
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
-tasks {
-
-    val sourceSets: SourceSetContainer by project
-
-    val sourcesJar by creating(Jar::class) {
-        dependsOn(JavaPlugin.CLASSES_TASK_NAME)
-        classifier = "sources"
-        from(sourceSets["main"].allSource)
-    }
-
-    val javadocJar by creating(Jar::class) {
-        dependsOn(JavaPlugin.JAVADOC_TASK_NAME)
-        classifier = "javadoc"
-        from(getByName("javadoc"))
-    }
-
-
-    val dokkaJar by creating(Jar::class) {
-        group = JavaBasePlugin.DOCUMENTATION_GROUP
-        description = "Assembles Kotlin docs with Dokka"
-        classifier = "javadoc"
-        from(getByName("dokka"))
-    }
-
-    artifacts {
-        add("archives", sourcesJar)
-        add("archives", dokkaJar)
-    }
-
-
-}
-
-task("writeNewPom") {
-    doLast {
-        maven.pom {
-            withGroovyBuilder {
-                "project" {
-                    // setProperty("inceptionYear", "2008")
-                    "licenses" {
-                        "license" {
-                            setProperty("name", "The Apache Software License, Version 2.0")
-                            setProperty("url", "http://www.apache.org/licenses/LICENSE-2.0.txt")
-                            setProperty("distribution", "repo")
-                        }
-                    }
-                }
-            }
-        }.writeTo("$buildDir/pom.xml")
-    }
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("EvoleqPublication"){
-            artifactId = Config.ProjectData.Configurations.artifactId
-            groupId = Config.ProjectData.group
-            from (components["java"])
-
-            artifact (tasks.getByName("sourcesJar")) {
-                classifier = "sources"
-            }
-
-            artifact (tasks.getByName("javadocJar")) {
-                classifier = "javadoc"
-            }
-
-            pom.withXml {
-                val root = asNode()
-                root.appendNode("description", "A declarative approach to application design based on the theory of dynamical systems")
-                root.appendNode("name", Config.ProjectData.Configurations.artifactId)
-                root.appendNode("url", "https://github.com/doctor-smith/evoleq.git")
-                root.children().addAll(maven.pom().dependencies)
-            }
-
-            pom {
-                developers{
-                    developer{
-                        id.set("drx")
-                        name.set("Dr. Florian Schmidt")
-                        email.set("schmidt@alpha-structure.com")
-                    }
-                }
-            }
-
-        }
-    }
-}
-
-
-
-
-bintray {
-    user = project.properties["bintray.user"] as String
-    key = project.properties["bintray.key"] as String
-
-    publish = true
-    override = true
-
-
-    pkg (delegateClosureOf<BintrayExtension.PackageConfig>{
-        repo = "maven"
-        name = "dynamics"
-        description = "A declarative approach to application design using the theory of dynamical systems"
-        //userOrg = user
-        vcsUrl = "https://bitbucket.org/dr-smith/evoleq.git"
-        setLabels("kotlin", "coroutine", "dynamical system", "recursive store", "evolution equation", "declarative", "functional")
-        setLicenses("Apache-2.0")
-
-        version (delegateClosureOf<BintrayExtension.VersionConfig>{
-            name = Config.ProjectData.Configurations.version
-            //desc = "build ${build.number}"
-            //released  = Date(System.currentTimeMillis())
-            gpg (delegateClosureOf<BintrayExtension.GpgConfig>{
-                sign = true
-            })
-        })
-    })
-
-}
-*/
